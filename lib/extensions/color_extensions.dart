3cceca467e64b585f401aa02e68d1d7f3112817d
Manoel de Souza Rocha Neto <manoelrocha.n93@gmail.com>
2023-10-02 07:24:56 -0300

    Fixes Color extension

--- lib/extensions/color_extensions.dart
+++ lib/extensions/color_extensions.dart
@@ -4,13 +4,15 @@ import 'package:quiz_app/extensions/string_extensions.dart';
 extension ColorExtensions on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-
-    if (hexString.isNullOrEmpty || hexString.isGreaterThan(7)) {
-      buffer.write('ff');
+    if (hexString.isNullOrEmpty) {
+      throw Exception('Hex Color String is null or empty');
     }
 
-    buffer.write(hexString.replaceFirst('#', ''));
+    final buffer = StringBuffer();
+
+    buffer.write('#');
+    buffer.write(hexString);
+
     return Color(int.parse(buffer.toString(), radix: 16));
   }
 }
