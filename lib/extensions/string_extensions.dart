dbbd6235013bc374807f174ee7172f65f14266d7
Manoel de Souza Rocha Neto <manoelrocha.n93@gmail.com>
2023-10-02 21:21:52 -0300

    Fixes fromHex string extension method

--- lib/extensions/string_extensions.dart
+++ lib/extensions/string_extensions.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+
 import './boolean_extensions.dart';
 
 extension StringExtensions on String? {
   bool get isNullOrEmpty => this == null || this!.isEmpty;
+  int get count => isNullOrEmpty ? 0 : this!.length;
 
   String capitalize({bool isTitle = false}) {
     if (isNullOrEmpty) {
@@ -20,16 +23,24 @@ extension StringExtensions on String? {
     return modifiedPhrase.trimRight();
   }
 
+  bool equals(int value) {
+    if (isNullOrEmpty) {
+      return false;
+    }
+
+    return this!.count == value;
+  }
+
   bool isGreaterThan(int value, {bool equal = false}) {
     if (isNullOrEmpty) {
       return false;
     }
 
     if (equal.isTrue) {
-      return this!.length >= value;
+      return this!.count >= value;
     }
 
-    return this!.length > value;
+    return this!.count > value;
   }
 
   bool isLessThan(int value, {bool equal = false}) {
@@ -38,11 +49,30 @@ extension StringExtensions on String? {
     }
 
     if (equal.isTrue) {
-      return this!.length <= value;
+      return this!.count <= value;
     }
 
-    return this!.length < value;
+    return this!.count < value;
   }
 
+  Color fromHex() {
+    var hexString = this;
 
+    if (hexString.isNullOrEmpty) {
+      throw Exception('Hex Color String is null or empty');
+    }
+
+    if (hexString!.startsWith('#')) {
+      hexString = hexString.substring(1);
+    }
+
+    final buffer = StringBuffer();
+
+    if (hexString.equals(6)) {
+      buffer.write('ff');
+    }
+
+    buffer.write(hexString);
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
 }
