36c226a33d50395af064e386e0892feb9222f8d2
Manoel de Souza Rocha Neto <manoelrocha.n93@gmail.com>
2023-10-05 07:24:39 -0300

    Adds SizedBox to DisplayImage to control image dimensions

--- lib/widgets/shared/display_image.dart
+++ lib/widgets/shared/display_image.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/widgets/shared/configs/image_path_variants.dart';
+
+import '../../extensions/string_extensions.dart';
+import '../../widgets/shared/configs/image_path_variants.dart';
 
 class DisplayImage extends StatelessWidget {
   final String imagePath;
+  final String color;
   final ImagePathVariant variant;
 
-  const DisplayImage({
-    required this.imagePath,
-    this.variant = ImagePathVariant.asset,
-    super.key
-  });
+  const DisplayImage(
+      {required this.imagePath,
+      this.color = '#FFFFFF',
+      this.variant = ImagePathVariant.asset,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
-    switch (variant) {
-      case ImagePathVariant.network:
-        return Image.network(imagePath);
-      
-      case ImagePathVariant.asset:
-      default:
-        return Image.asset(imagePath);
+    Widget buildImage({
+      required String imagePath,
+      required ImagePathVariant variant,
+      String color = '#FFFFFF',
+    }) {
+      switch (variant) {
+        case ImagePathVariant.network:
+          return Image.network(
+            imagePath,
+            color: color.fromHex(),
+          );
+
+        case ImagePathVariant.asset:
+        default:
+          return Image.asset(
+            imagePath,
+            color: color.fromHex(),
+          );
+      }
     }
+
+    return SizedBox(
+      height: 200,
+      width: 200,
+      child: buildImage(imagePath: imagePath, variant: variant),
+    );
   }
 }
